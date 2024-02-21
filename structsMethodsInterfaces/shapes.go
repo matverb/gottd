1a27d2eb8464dafc7382415cc54b534238b5ea30
unknown <mathias@verboven.be>
2024-02-21 13:25:16 +0100

    pointers + maps+ dependency

--- structsMethodsInterfaces/shapes.go
+++ structsMethodsInterfaces/shapes.go
@@ -16,16 +16,16 @@ type Triangle struct {
 	Height float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
 func (t Triangle) Area() float64 {
 	return (t.Base * t.Height) * 0.5
 }
 
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
-}
-
-type Circle struct {
-	Radius float64
 }
 
 func (c Circle) Area() float64 {
